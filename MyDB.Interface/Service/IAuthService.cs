acf7dde31830031f5d9196712b3f6d584c728114
waterpare833 <waterpare833@naver.com>
2025-06-21 17:50:41 +0900

    add Is_username_taken method in my-db

--- MyDB.Interface/Service/IAuthService.cs
+++ MyDB.Interface/Service/IAuthService.cs
@@ -4,4 +4,5 @@ public interface IAuthService : IService<IAuthService>
 {
     UnaryResult<bool> Register(string username, string password);
     UnaryResult<bool> Login(string username, string password);
+    UnaryResult<bool> Is_username_taken(string username, string password);
 }
--- MyDB/Service/AuthService.cs
+++ MyDB/Service/AuthService.cs
@@ -2,26 +2,32 @@
 
 public class AuthService : ServiceBase<IAuthService>, IAuthService
 {
-    private readonly IUserRepository repo;
+    private readonly IUserRepository user_repo;
 
-    public AuthService(IUserRepository repo) => this.repo = repo;
+    public AuthService(IUserRepository user_repo) => this.user_repo = user_repo;
 
     public async UnaryResult<bool> Register(string username, string password)
     {
         var hashed_password = hash_password(password);
-        return await this.repo.Add_user(username, hashed_password);
+        return await this.user_repo.Add_user(username, hashed_password);
     }
 
     public async UnaryResult<bool> Login(string username, string password)
     {
-        var user = await this.repo.Get_user(username);
+        var user = await this.user_repo.Get_user(username);
         if (user is null) return false;
 
         var hash = hash_password(password);
         return hash == user.Password_hash;
     }
 
-    private static string hash_password(string password)
+    public async UnaryResult<bool> Is_username_taken(string username, string password)
+    {
+        var user = await this.user_repo.Get_user(username);
+        return user != null;
+    }
+
+    private string hash_password(string password)
     {
         using var sha = SHA256.Create();
         var bytes = Encoding.UTF8.GetBytes(password);
